65490e958392ac83fc50a354bcd71dd11b911572
Rian-Borah <44469041+Rian-Borah@users.noreply.github.com>
2019-11-20 06:42:03 +0530

    added Arduino code
    For controlling electronics

---
+++ Electronics Control/nCrypt.ino
@@ -0,0 +1,545 @@
+
+/*
+ * Image Processing based Gesture-controlled Arduino Home Automation Demonstration
+ * 
+ * Anmol Singh and Rian Borah, Delhi Public School, RK Puram
+ * 
+ * Program 2: Slave Arduino Mega 2560
+ * 
+ */
+ 
+// _____________________________
+
+#include <Wire.h>
+
+#include <SparkFun_TB6612.h> 
+
+#define AIN1 22 
+
+#define AIN2 24
+
+#define PWMA 5
+
+#define STBY 23
+
+const int offsetA = 1;
+
+Motor motor1 = Motor(AIN1, AIN2, PWMA, offsetA, STBY);
+
+volatile int counter = 0;
+
+// _____________________________
+
+int led1 = 53;
+
+int led2 = 52;
+
+int led3 = 50;
+
+int led4 = 51;
+
+// _____________________________
+
+int latchPin = 39;
+
+int clockPin = 37;
+
+int dataPin = 38;
+
+// _____________________________
+
+int R = 2;
+
+int G = 3;
+
+int B = 4;
+
+// _____________________________
+
+// Brightness Control (first two) or Toggle Control (last two)
+
+char deviceArray[5] = {'L', 'M', 'R', 'S', 'B'};
+//
+int numEffectiveStates = 5;
+
+//char deviceArray[3] = {'L', 'M', 'R'};
+//
+//int numEffectiveStates = 3;  
+
+int deviceID = 0;
+
+// _____________________________
+
+int currentBrightness = 50;
+
+int currentSpeed = 20;
+
+int currentMode = 1;
+
+int defaultLEDStep = 20;
+
+int defaultMotorStep = 5;
+
+int registerBrightness = 200;
+
+int previousBrightness = 0;
+
+int previousSpeed = 0;
+
+// _____________________________
+
+void setup() {
+  // put your setup code here, to run once:
+
+  Wire.begin(9);
+
+  pinMode(R, OUTPUT);
+  
+  pinMode(G, OUTPUT);
+  
+  pinMode(B, OUTPUT);
+  
+  pinMode(18, INPUT);
+
+  pinMode(led1, OUTPUT);
+
+  pinMode(led2, OUTPUT);
+
+  pinMode(led3, OUTPUT);
+  
+  pinMode(led4, OUTPUT);
+  
+  pinMode(13, OUTPUT);
+  
+  digitalWrite(13, LOW);
+  
+  attachInterrupt(digitalPinToInterrupt(18), changeState, RISING);
+
+  changeState();
+
+  pinMode(latchPin, OUTPUT);
+
+  pinMode(dataPin, OUTPUT);  
+  
+  pinMode(clockPin, OUTPUT);
+
+  pinMode(6, OUTPUT);
+
+  analogWrite(6, registerBrightness);
+
+  Serial.begin(9600);
+
+  Wire.onReceive(receiveEvent);
+
+}
+
+void loop() {
+
+while(deviceID == 0) {
+
+  resetMotor();
+
+  resetLights();
+
+  Serial.println("Controlling RGB LED");
+
+  
+}
+
+while(deviceID == 1) {
+
+  resetRGBLED();
+
+  resetLights();
+
+  Serial.println("Controlling Motor");
+  
+}
+
+while(deviceID >= 2) {
+
+  resetRGBLED();
+
+  resetMotor();
+
+  Serial.println("Controlling Lights");
+  
+}
+
+}
+
+void changeState(){
+
+  if((counter % numEffectiveStates) == 0){
+  
+    digitalWrite(led1, HIGH);
+    
+    digitalWrite(led2, LOW);
+    
+    digitalWrite(led3, LOW);
+
+    digitalWrite(led4, LOW);
+  
+  }
+  
+  else if((counter % numEffectiveStates) == 1){
+  
+    digitalWrite(led1, LOW);
+    
+    digitalWrite(led2, HIGH);
+    
+    digitalWrite(led3, LOW);
+    
+    digitalWrite(led4, LOW);
+  
+  }
+  
+  else if ((counter % numEffectiveStates) == 2){
+
+    if(numEffectiveStates == 3){
+
+        digitalWrite(led1, LOW);
+    
+        digitalWrite(led2, LOW);
+    
+        digitalWrite(led3, HIGH);
+    
+        digitalWrite(led4, HIGH);
+      
+    }
+
+    else {
+
+      digitalWrite(led1, LOW);
+    
+      digitalWrite(led2, LOW);
+    
+      digitalWrite(led3, HIGH);
+    
+      digitalWrite(led4, LOW);
+      
+    }
+  
+    
+  }
+
+  else if ((counter % numEffectiveStates) == 3){
+  
+    digitalWrite(led1, LOW);
+    
+    digitalWrite(led2, LOW);
+    
+    digitalWrite(led3, LOW);
+    
+    digitalWrite(led4, HIGH);
+  
+  }
+  
+  else {
+  
+    digitalWrite(led1, LOW);
+    
+    digitalWrite(led2, LOW);
+    
+    digitalWrite(led3, HIGH);
+    
+    digitalWrite(led4, HIGH);
+  }
+ 
+  counter += 1;
+
+  deviceID = (counter - 1) % numEffectiveStates;  
+
+}
+
+void changeRGBLEDBrightness(int newBrightness) {
+
+  if(newBrightness > 255) {
+
+    newBrightness = 255;    
+
+  }
+
+  else if(newBrightness < 0) {
+
+    newBrightness = 0;  
+  
+  }  
+
+  analogWrite(R, newBrightness);
+
+  analogWrite(G, newBrightness);
+
+  analogWrite(B, newBrightness);
+
+}
+
+int mapSpeed(int s){
+
+  return s * 60/78;
+
+}
+
+int changeMotorSpeed(int newSpeed) {
+
+  if(newSpeed > 255){
+
+    newSpeed = 255;
+    
+  }
+
+  else if(newSpeed < 0){
+
+    newSpeed = 0;
+    
+  }
+
+  currentSpeed = newSpeed;
+  
+  motor1.drive(mapSpeed(newSpeed));
+
+}
+
+void setLightingMode(int mode){
+
+  if(numEffectiveStates == 3) {
+
+  if(mode == 0) {
+
+    digitalWrite(latchPin, LOW);
+
+    shiftOut(dataPin, clockPin, LSBFIRST, 0);
+
+    digitalWrite(latchPin, HIGH);
+  
+  }
+
+  else if(mode == 1) {
+
+    digitalWrite(latchPin, LOW);
+
+    shiftOut(dataPin, clockPin, LSBFIRST, 0b11110000);
+
+    digitalWrite(latchPin, HIGH);
+
+  }  
+
+  else if(mode == 2) {
+
+    digitalWrite(latchPin, LOW);
+
+    shiftOut(dataPin, clockPin, LSBFIRST, 0b00001111);
+
+    digitalWrite(latchPin, HIGH);
+
+  }
+
+  else {
+
+    digitalWrite(latchPin, LOW);
+
+    shiftOut(dataPin, clockPin, LSBFIRST, 0b11111111);
+
+    digitalWrite(latchPin, HIGH);
+
+  }
+
+  }
+
+  else {
+
+    digitalWrite(latchPin, LOW);
+
+    shiftOut(dataPin, clockPin, LSBFIRST, 0);
+
+    digitalWrite(latchPin, HIGH);
+
+    if(mode > 255){
+
+      mode = 255;
+      
+    }
+
+    else if(mode < 0){
+
+      mode = 0;
+      
+    }
+
+    if(deviceID == 2){
+
+    digitalWrite(latchPin, LOW);
+
+    shiftOut(dataPin, clockPin, LSBFIRST, 0b11110000);
+
+    digitalWrite(latchPin, HIGH);
+
+    }
+
+    else if (deviceID == 3){
+
+      
+    digitalWrite(latchPin, LOW);
+
+    shiftOut(dataPin, clockPin, LSBFIRST, 0b00001111);
+
+    digitalWrite(latchPin, HIGH);        
+      
+    }
+
+    else {
+      
+    digitalWrite(latchPin, LOW);
+
+    shiftOut(dataPin, clockPin, LSBFIRST, 0b11111111);
+
+    digitalWrite(latchPin, HIGH);
+     
+  }
+
+    analogWrite(6, mode);
+
+    Serial.println(mode);
+  
+  }
+
+}
+
+void transferControl(char change) {
+
+  Serial.println(deviceID);
+
+  if(deviceArray[deviceID] == 'L') {
+
+      if(change == '+'){
+
+        currentBrightness += defaultLEDStep;
+
+      }
+
+      else if(change == '-'){
+
+        currentBrightness -= defaultLEDStep; 
+    
+      }
+
+      changeRGBLEDBrightness(currentBrightness);
+
+  }
+
+  else if(deviceArray[deviceID] == 'M') {
+
+      if(change == '+'){
+
+        currentSpeed += defaultMotorStep;
+
+      }
+
+      else if(change == '-'){
+
+        currentSpeed -= defaultMotorStep; 
+    
+      }
+
+      changeMotorSpeed(currentSpeed);
+
+  }
+
+  else {
+
+    if(numEffectiveStates == 3){
+
+     if(change == '+'){
+
+        currentMode += 1;
+
+      }
+
+      else if(change == '-'){
+
+        currentMode -= 1; 
+    
+      }
+
+      currentMode = currentMode % 4;
+
+      setLightingMode(currentMode);
+
+  }
+
+  else {
+
+     if(change == '+'){
+
+        registerBrightness -= defaultLEDStep;
+
+      }
+
+      else if(change == '-'){
+
+        registerBrightness += defaultLEDStep; 
+    
+      }
+
+      setLightingMode(registerBrightness);
+    
+    }
+    
+  }
+
+}
+
+void resetMotor(){
+  
+  motor1.drive(0);
+
+}
+
+void resetRGBLED(){
+
+  analogWrite(R, 0);
+
+  analogWrite(G, 0);
+
+  analogWrite(B, 0);
+  
+}
+
+void resetLights(){
+
+    digitalWrite(latchPin, LOW);
+
+    shiftOut(dataPin, clockPin, LSBFIRST, 0);
+
+    digitalWrite(latchPin, HIGH);
+
+    delay(20);
+  
+}
+
+void receiveEvent(){
+
+  byte a = Wire.read();
+
+  Serial.println(a);
+
+  if(a == 45){
+
+    transferControl('-');
+    
+  }
+
+  else if(a == 43){
+
+    transferControl('+');
+    
+  }
+
+  else {
+
+    transferControl('.');
+    
+  }
+  
+}
+
---
+++ Electronics Control/nCrypt_unoMaster.ino
@@ -0,0 +1,86 @@
+/*
+ * Image Processing based Gesture-controlled Arduino Home Automation Demonstration
+ * 
+ * Anmol Singh and Rian Borah, Delhi Public School, RK Puram
+ * 
+ * Program 1: Master Arduino Uno
+ * 
+ */
+ 
+#include <SPI.h>  
+#include <Pixy.h>
+#include <Wire.h>
+int slaveAddress = 9;
+Pixy pixy;
+bool b = 0;
+void setup()
+{
+  Wire.begin();
+  Serial.begin(9600);
+  pixy.init();
+  pixy.setLED(200, 200, 200);
+}
+
+void loop()
+{ 
+while(1){
+  getData();
+  while(Serial.available()){
+  char c = Serial.read();
+  if (c == '+'){
+  Wire.beginTransmission(9);
+  Wire.write('+'); 
+  Wire.endTransmission();
+  }
+ else if(c == '-'){
+  Wire.beginTransmission(9);
+  Wire.write('-'); 
+  Wire.endTransmission(); 
+ } 
+ else if(c == 'x'){
+  Wire.beginTransmission(9);
+  Wire.write('x'); 
+  Wire.endTransmission();
+  b = 1;
+  break;
+ }
+  getData();
+}
+if(b == 1){
+  b  = 0;
+  break;
+}
+}
+delay(100);
+Serial.println("Stop");
+Serial.readString();
+Serial.flush();
+delay(500);
+}
+void getData(){
+    static int i = 0;
+  int j;
+  uint16_t blocks;
+  char buf[32]; 
+  blocks = pixy.getBlocks();
+  if (blocks)
+  {
+    i++;
+    int X = 0;
+    int Y = 0;
+    int counter = 0;
+      for (j=0; j<blocks; j++)
+      {
+        X += pixy.blocks[j].x;
+        Y += pixy.blocks[j].y;
+        counter++;
+      }
+      X = (int) X/counter;
+      Y = (int) Y/counter;
+      Serial.print("0\t");
+      Serial.print((String) X);
+      Serial.print('\t');
+      Serial.print((String) Y);
+      Serial.println();
+}
+}
